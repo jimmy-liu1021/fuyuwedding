c378fa7991f5b8d143963fd62c792074389a4819
jimmy-liu1021 <80306173+jimmy-liu1021@users.noreply.github.com>
2026-03-28 01:31:13 +0800

    add attendance
    Add a column for attendance

--- data.js
+++ data.js
@@ -3,870 +3,1015 @@
     "name": "孔正富",
     "table": "3",
     "stroke": 4,
-    "hasGift": false
+    "hasGift": false,
+    "attendance": 2
   },
   {
     "name": "孔紀秋梅",
     "table": "主",
     "stroke": 4,
-    "hasGift": false
+    "hasGift": false,
+    "attendance": 1
   },
   {
     "name": "孔麗芳",
     "table": "8",
     "stroke": 4,
-    "hasGift": false
+    "hasGift": false,
+    "attendance": 1
   },
   {
     "name": "王平安",
     "table": "12",
     "stroke": 4,
-    "hasGift": true
+    "hasGift": true,
+    "attendance": 1
   },
   {
     "name": "王俞婷",
     "table": "29",
     "stroke": 4,
-    "hasGift": true
+    "hasGift": true,
+    "attendance": 1
   },
   {
     "name": "王鈺",
     "table": "28",
     "stroke": 4,
-    "hasGift": true
+    "hasGift": true,
+    "attendance": 2
   },
   {
     "name": "王榮慶",
     "table": "16",
     "stroke": 4,
-    "hasGift": false
+    "hasGift": false,
+    "attendance": 2
   },
   {
     "name": "王德忠",
     "table": "23",
     "stroke": 4,
-    "hasGift": false
+    "hasGift": false,
+    "attendance": 2
   },
   {
     "name": "王錦松",
     "table": "21",
     "stroke": 4,
-    "hasGift": false
+    "hasGift": false,
+    "attendance": 1
   },
   {
     "name": "古榮昌",
     "table": "10",
     "stroke": 5,
-    "hasGift": false
+    "hasGift": false,
+    "attendance": 2
   },
   {
     "name": "朱一正",
     "table": "20",
     "stroke": 6,
-    "hasGift": false
+    "hasGift": false,
+    "attendance": 2
   },
   {
     "name": "朱展輝",
     "table": "12",
     "stroke": 6,
-    "hasGift": true
+    "hasGift": true,
+    "attendance": 2
   },
   {
     "name": "何駿惠",
     "table": "16",
     "stroke": 7,
-    "hasGift": false
+    "hasGift": false,
+    "attendance": 2
   },
   {
     "name": "余再得",
     "table": "22",
     "stroke": 7,
-    "hasGift": false
+    "hasGift": false,
+    "attendance": 2
   },
   {
     "name": "吳光裕",
     "table": "20",
     "stroke": 7,
-    "hasGift": false
+    "hasGift": false,
+    "attendance": 1
   },
   {
     "name": "吳志明",
     "table": "1",
     "stroke": 7,
-    "hasGift": false
+    "hasGift": false,
+    "attendance": 2
   },
   {
     "name": "吳孟玲",
     "table": "28",
     "stroke": 7,
-    "hasGift": true
+    "hasGift": true,
+    "attendance": 1
   },
   {
     "name": "吳秉鴻",
     "table": "8",
     "stroke": 7,
-    "hasGift": false
+    "hasGift": false,
+    "attendance": 1
   },
   {
     "name": "吳阿禮",
     "table": "17",
     "stroke": 7,
-    "hasGift": false
+    "hasGift": false,
+    "attendance": 2
   },
   {
     "name": "吳俊伯",
     "table": "8",
     "stroke": 7,
-    "hasGift": false
+    "hasGift": false,
+    "attendance": 1
   },
   {
     "name": "呂元庭",
     "table": "9",
     "stroke": 7,
-    "hasGift": false
+    "hasGift": false,
+    "attendance": 2
   },
   {
     "name": "呂佳霖",
     "table": "6",
     "stroke": 7,
-    "hasGift": false
+    "hasGift": false,
+    "attendance": 1
   },
   {
     "name": "呂昆憲",
     "table": "3",
     "stroke": 7,
-    "hasGift": false
+    "hasGift": false,
+    "attendance": 2
   },
   {
     "name": "呂翼純",
     "table": "9",
     "stroke": 7,
-    "hasGift": false
+    "hasGift": false,
+    "attendance": 1
   },
   {
     "name": "宋玉祥",
     "table": "23",
     "stroke": 7,
-    "hasGift": false
+    "hasGift": false,
+    "attendance": 4
   },
   {
     "name": "李正明",
     "table": "25",
     "stroke": 7,
-    "hasGift": false
+    "hasGift": false,
+    "attendance": 1
   },
   {
     "name": "李宗璇",
     "table": "27",
     "stroke": 7,
-    "hasGift": true
+    "hasGift": true,
+    "attendance": 2
   },
   {
     "name": "李承翰",
     "table": "8",
     "stroke": 7,
-    "hasGift": false
+    "hasGift": false,
+    "attendance": 4
   },
   {
     "name": "李昭鋒",
     "table": "11",
     "stroke": 7,
-    "hasGift": false
+    "hasGift": false,
+    "attendance": 2
   },
   {
     "name": "李啟宏",
     "table": "15",
     "stroke": 7,
-    "hasGift": true
+    "hasGift": true,
+    "attendance": 1
   },
   {
     "name": "李晴雯",
     "table": "12",
     "stroke": 7,
-    "hasGift": true
+    "hasGift": true,
+    "attendance": 1
   },
   {
     "name": "李毓祥",
     "table": "12",
     "stroke": 7,
-    "hasGift": true
+    "hasGift": true,
+    "attendance": 1
   },
   {
     "name": "李銘祥",
     "table": "12",
     "stroke": 7,
-    "hasGift": true
+    "hasGift": true,
+    "attendance": 1
   },
   {
     "name": "李曉雯",
     "table": "29",
     "stroke": 7,
-    "hasGift": true
+    "hasGift": true,
+    "attendance": 3
   },
   {
     "name": "辛佩娟",
     "table": "13",
     "stroke": 7,
-    "hasGift": true
+    "hasGift": true,
+    "attendance": 1
   },
   {
     "name": "周宗霖",
     "table": "15",
     "stroke": 8,
-    "hasGift": true
+    "hasGift": true,
+    "attendance": 2
   },
   {
     "name": "周濟忠",
     "table": "9",
     "stroke": 8,
-    "hasGift": false
+    "hasGift": false,
+    "attendance": 2
   },
   {
     "name": "明政車業",
     "table": "23",
     "stroke": 8,
-    "hasGift": false
+    "hasGift": false,
+    "attendance": 2
   },
   {
     "name": "林三義",
     "table": "10",
     "stroke": 8,
-    "hasGift": false
+    "hasGift": false,
+    "attendance": 2
   },
   {
     "name": "林士哲",
     "table": "15",
     "stroke": 8,
-    "hasGift": true
+    "hasGift": true,
+    "attendance": 1
   },
   {
     "name": "林禾穎",
     "table": "21",
     "stroke": 8,
-    "hasGift": false
+    "hasGift": false,
+    "attendance": 1
   },
   {
     "name": "林佑任",
     "table": "13",
     "stroke": 8,
-    "hasGift": true
+    "hasGift": true,
+    "attendance": 1
   },
   {
     "name": "林長入",
     "table": "21",
     "stroke": 8,
-    "hasGift": false
+    "hasGift": false,
+    "attendance": 2
   },
   {
     "name": "林啟章",
     "table": "1",
     "stroke": 8,
-    "hasGift": false
+    "hasGift": false,
+    "attendance": 4
   },
   {
     "name": "林達清",
     "table": "17",
     "stroke": 8,
-    "hasGift": false
+    "hasGift": false,
+    "attendance": 2
   },
   {
     "name": "邱業志",
     "table": "13",
     "stroke": 8,
-    "hasGift": true
+    "hasGift": true,
+    "attendance": 1
   },
   {
     "name": "侯凱文",
     "table": "9",
     "stroke": 9,
-    "hasGift": false
+    "hasGift": false,
+    "attendance": 2
   },
   {
     "name": "侯顯昇",
     "table": "3",
     "stroke": 9,
-    "hasGift": false
+    "hasGift": false,
+    "attendance": 2
   },
   {
     "name": "姚明宏",
     "table": "17",
     "stroke": 9,
-    "hasGift": false
+    "hasGift": false,
+    "attendance": 2
   },
   {
     "name": "封居和",
     "table": "29",
     "stroke": 9,
-    "hasGift": true
+    "hasGift": true,
+    "attendance": 2
   },
   {
     "name": "柯志雄",
     "table": "19",
     "stroke": 9,
-    "hasGift": false
+    "hasGift": false,
+    "attendance": 2
   },
   {
     "name": "柯河協",
     "table": "19",
     "stroke": 9,
-    "hasGift": false
+    "hasGift": false,
+    "attendance": 2
   },
   {
     "name": "洪秋菊",
     "table": "20",
     "stroke": 9,
-    "hasGift": false
+    "hasGift": false,
+    "attendance": 2
   },
   {
     "name": "洪啟聰",
     "table": "11",
     "stroke": 9,
-    "hasGift": false
+    "hasGift": false,
+    "attendance": 2
   },
   {
     "name": "洪慶昌",
     "table": "10",
     "stroke": 9,
-    "hasGift": false
+    "hasGift": false,
+    "attendance": 3
   },
   {
     "name": "紀素蘭",
     "table": "3",
     "stroke": 9,
-    "hasGift": false
+    "hasGift": false,
+    "attendance": 1
   },
   {
     "name": "翁晉源",
     "table": "26",
     "stroke": 10,
-    "hasGift": false
+    "hasGift": false,
+    "attendance": 2
   },
   {
     "name": "徐紹謙",
     "table": "9",
     "stroke": 10,
-    "hasGift": false
+    "hasGift": false,
+    "attendance": 3
   },
   {
     "name": "張永林",
     "table": "主",
     "stroke": 11,
-    "hasGift": false
+    "hasGift": false,
+    "attendance": 1
   },
   {
     "name": "張宇仁",
     "table": "11",
     "stroke": 11,
-    "hasGift": false
+    "hasGift": false,
+    "attendance": 1
   },
   {
     "name": "張孝瑄",
     "table": "5",
     "stroke": 11,
-    "hasGift": true
+    "hasGift": true,
+    "attendance": 1
   },
   {
     "name": "張秀屏",
     "table": "5",
     "stroke": 11,
-    "hasGift": false
+    "hasGift": false,
+    "attendance": 1
   },
   {
     "name": "張育誠",
     "table": "3",
     "stroke": 11,
-    "hasGift": false
+    "hasGift": false,
+    "attendance": 3
   },
   {
     "name": "張宗德",
     "table": "18",
     "stroke": 11,
-    "hasGift": false
+    "hasGift": false,
+    "attendance": 1
   },
   {
     "name": "張美淑",
     "table": "10",
     "stroke": 11,
-    "hasGift": false
+    "hasGift": false,
+    "attendance": 1
   },
   {
     "name": "張書鳴",
     "table": "7",
     "stroke": 11,
-    "hasGift": false
+    "hasGift": false,
+    "attendance": 1
   },
   {
     "name": "張國勤",
     "table": "10",
     "stroke": 11,
-    "hasGift": false
+    "hasGift": false,
+    "attendance": 2
   },
   {
     "name": "張景崑",
     "table": "18",
     "stroke": 11,
-    "hasGift": false
+    "hasGift": false,
+    "attendance": 3
   },
   {
     "name": "張曉萍",
     "table": "1",
     "stroke": 11,
-    "hasGift": false
+    "hasGift": false,
+    "attendance": 1
   },
   {
     "name": "莊永來",
     "table": "11",
     "stroke": 11,
-    "hasGift": false
+    "hasGift": false,
+    "attendance": 2
   },
   {
     "name": "莊祐齊",
     "table": "28",
     "stroke": 11,
-    "hasGift": true
+    "hasGift": true,
+    "attendance": 2
   },
   {
     "name": "許有發",
     "table": "25",
     "stroke": 11,
-    "hasGift": false
+    "hasGift": false,
+    "attendance": 3
   },
   {
     "name": "許志偉",
     "table": "12",
     "stroke": 11,
-    "hasGift": true
+    "hasGift": true,
+    "attendance": 1
   },
   {
     "name": "郭盈瑩",
     "table": "13",
     "stroke": 11,
-    "hasGift": true
+    "hasGift": true,
+    "attendance": 1
   },
   {
     "name": "郭貞妤",
     "table": "6",
     "stroke": 11,
-    "hasGift": false
+    "hasGift": false,
+    "attendance": 1
   },
   {
     "name": "郭慶松",
     "table": "22",
     "stroke": 11,
-    "hasGift": false
+    "hasGift": false,
+    "attendance": 4
   },
   {
     "name": "陳文英",
     "table": "5",
     "stroke": 11,
-    "hasGift": true
+    "hasGift": true,
+    "attendance": 1
   },
   {
     "name": "陳正忠",
     "table": "18",
     "stroke": 11,
-    "hasGift": false
+    "hasGift": false,
+    "attendance": 2
   },
   {
     "name": "陳民智",
     "table": "25",
     "stroke": 11,
-    "hasGift": false
+    "hasGift": false,
+    "attendance": 2
   },
   {
     "name": "陳永洲",
     "table": "19",
     "stroke": 11,
-    "hasGift": false
+    "hasGift": false,
+    "attendance": 1
   },
   {
     "name": "陳俊男",
     "table": "20",
     "stroke": 11,
-    "hasGift": false
+    "hasGift": false,
+    "attendance": 1
   },
   {
     "name": "陳奕臻",
     "table": "28",
     "stroke": 11,
-    "hasGift": true
+    "hasGift": true,
+    "attendance": 1
   },
   {
     "name": "陳柏亨",
     "table": "28",
     "stroke": 11,
-    "hasGift": true
+    "hasGift": true,
+    "attendance": 2
   },
   {
     "name": "陳素貞",
     "table": "1",
     "stroke": 11,
-    "hasGift": false
+    "hasGift": false,
+    "attendance": 1
   },
   {
     "name": "陳聖馗",
     "table": "12",
     "stroke": 11,
-    "hasGift": true
+    "hasGift": true,
+    "attendance": 2
   },
   {
     "name": "陳裕生",
     "table": "15",
     "stroke": 11,
-    "hasGift": true
+    "hasGift": true,
+    "attendance": 1
   },
   {
     "name": "彭永富",
     "table": "18",
     "stroke": 12,
-    "hasGift": false
+    "hasGift": false,
+    "attendance": 2
   },
   {
     "name": "游天下",
     "table": "2",
     "stroke": 12,
-    "hasGift": false
+    "hasGift": false,
+    "attendance": 4
   },
   {
     "name": "游志成",
     "table": "2",
     "stroke": 12,
-    "hasGift": false
+    "hasGift": false,
+    "attendance": 2
   },
   {
     "name": "游武發",
     "table": "主",
     "stroke": 12,
-    "hasGift": false
+    "hasGift": false,
+    "attendance": 2
   },
   {
     "name": "游俊勝",
     "table": "7",
     "stroke": 12,
-    "hasGift": false
+    "hasGift": false,
+    "attendance": 9
   },
   {
     "name": "游俊惠",
     "table": "2",
     "stroke": 12,
-    "hasGift": false
+    "hasGift": false,
+    "attendance": 2
   },
   {
     "name": "游峻林",
     "table": "2",
     "stroke": 12,
-    "hasGift": false
+    "hasGift": false,
+    "attendance": 2
   },
   {
     "name": "游淑芬",
     "table": "11",
     "stroke": 12,
-    "hasGift": false
+    "hasGift": false,
+    "attendance": 1
   },
   {
     "name": "湯有成",
     "table": "20",
     "stroke": 12,
-    "hasGift": false
+    "hasGift": false,
+    "attendance": 2
   },
   {
     "name": "黃世雄",
     "table": "26",
     "stroke": 12,
-    "hasGift": false
+    "hasGift": false,
+    "attendance": 1
   },
   {
     "name": "黃成平",
     "table": "26",
     "stroke": 12,
-    "hasGift": false
+    "hasGift": false,
+    "attendance": 3
   },
   {
     "name": "黃秋美",
     "table": "16",
     "stroke": 12,
-    "hasGift": false
+    "hasGift": false,
+    "attendance": 1
   },
   {
     "name": "黃美芬",
     "table": "13",
     "stroke": 12,
-    "hasGift": true
+    "hasGift": true,
+    "attendance": 1
   },
   {
     "name": "黃美珍",
     "table": "20",
     "stroke": 12,
-    "hasGift": false
+    "hasGift": false,
+    "attendance": 1
   },
   {
     "name": "黃振鈞",
     "table": "主",
     "stroke": 12,
-    "hasGift": false
+    "hasGift": false,
+    "attendance": 1
   },
   {
     "name": "黃健航",
     "table": "15",
     "stroke": 12,
-    "hasGift": true
+    "hasGift": true,
+    "attendance": 2
   },
   {
     "name": "黃崇華",
     "table": "6",
     "stroke": 12,
-    "hasGift": false
+    "hasGift": false,
+    "attendance": 2
   },
   {
     "name": "黃意婷",
     "table": "27",
     "stroke": 12,
-    "hasGift": true
+    "hasGift": true,
+    "attendance": 2
   },
   {
     "name": "黃福源",
     "table": "17",
     "stroke": 12,
-    "hasGift": false
+    "hasGift": false,
+    "attendance": 2
   },
   {
     "name": "黃馨儀",
     "table": "27",
     "stroke": 12,
-    "hasGift": true
+    "hasGift": true,
+    "attendance": 1
   },
   {
     "name": "楊王元廷",
     "table": "28",
     "stroke": 13,
-    "hasGift": true
+    "hasGift": true,
+    "attendance": 1
   },
   {
     "name": "楊保生",
     "table": "17",
     "stroke": 13,
-    "hasGift": false
+    "hasGift": false,
+    "attendance": 1
   },
   {
     "name": "楊益彰",
     "table": "1",
     "stroke": 13,
-    "hasGift": false
+    "hasGift": false,
+    "attendance": 2
   },
   {
     "name": "楊清雄",
     "table": "19",
     "stroke": 13,
-    "hasGift": false
+    "hasGift": false,
+    "attendance": 2
   },
   {
     "name": "楊智捷",
     "table": "15",
     "stroke": 13,
-    "hasGift": true
+    "hasGift": true,
+    "attendance": 1
   },
   {
     "name": "葉永輝",
     "table": "17",
     "stroke": 13,
-    "hasGift": false
+    "hasGift": false,
+    "attendance": 1
   },
   {
     "name": "賈志浩",
     "table": "29",
     "stroke": 13,
-    "hasGift": true
+    "hasGift": true,
+    "attendance": 2
   },
   {
     "name": "鄒嘉棋",
     "table": "16",
     "stroke": 13,
-    "hasGift": false
+    "hasGift": false,
+    "attendance": 1
   },
   {
     "name": "廖啟亨",
     "table": "28",
     "stroke": 14,
-    "hasGift": true
+    "hasGift": true,
+    "attendance": 1
   },
   {
     "name": "廖燈村",
     "table": "21",
     "stroke": 14,
-    "hasGift": false
+    "hasGift": false,
+    "attendance": 2
   },
   {
     "name": "劉文能",
     "table": "25",
     "stroke": 15,
-    "hasGift": false
+    "hasGift": false,
+    "attendance": 3
   },
   {
     "name": "劉志宗",
     "table": "18",
     "stroke": 15,
-    "hasGift": false
+    "hasGift": false,
+    "attendance": 2
   },
   {
     "name": "劉邦宏",
     "table": "主",
     "stroke": 15,
-    "hasGift": false
+    "hasGift": false,
+    "attendance": 1
   },
   {
     "name": "劉佳偉",
     "table": "5",
     "stroke": 15,
-    "hasGift": true
+    "hasGift": true,
+    "attendance": 1
   },
   {
     "name": "劉晉華",
     "table": "8",
     "stroke": 15,
-    "hasGift": false
+    "hasGift": false,
+    "attendance": 2
   },
   {
     "name": "劉婉苓",
     "table": "8",
     "stroke": 15,
-    "hasGift": false
+    "hasGift": false,
+    "attendance": 1
   },
   {
     "name": "劉達德",
     "table": "19",
     "stroke": 15,
-    "hasGift": false
+    "hasGift": false,
+    "attendance": 2
   },
   {
     "name": "劉德陽",
     "table": "5",
     "stroke": 15,
-    "hasGift": false
+    "hasGift": false,
+    "attendance": 4
   },
   {
     "name": "劉興漢",
     "table": "6",
     "stroke": 15,
-    "hasGift": false
+    "hasGift": false,
+    "attendance": 4
   },
   {
     "name": "劉馥瑩",
     "table": "5",
     "stroke": 15,
-    "hasGift": false
+    "hasGift": false,
+    "attendance": 1
   },
   {
     "name": "歐進雄",
     "table": "26",
     "stroke": 15,
-    "hasGift": false
+    "hasGift": false,
+    "attendance": 2
   },
   {
     "name": "蔡正昌",
     "table": "6",
     "stroke": 15,
-    "hasGift": false
+    "hasGift": false,
+    "attendance": 2
   },
   {
     "name": "蔡明信",
     "table": "15",
     "stroke": 15,
-    "hasGift": true
+    "hasGift": true,
+    "attendance": 1
   },
   {
     "name": "蔡昕翰",
     "table": "27",
     "stroke": 15,
-    "hasGift": true
+    "hasGift": true,
+    "attendance": 1
   },
   {
     "name": "蔡啟明",
     "table": "21",
     "stroke": 15,
-    "hasGift": false
+    "hasGift": false,
+    "attendance": 2
   },
   {
     "name": "蔡智閱",
     "table": "13",
     "stroke": 15,
-    "hasGift": true
+    "hasGift": true,
+    "attendance": 1
   },
   {
     "name": "蔡瑞珍",
     "table": "13",
     "stroke": 15,
-    "hasGift": true
+    "hasGift": true,
+    "attendance": 1
   },
   {
     "name": "蔣瑞融",
     "table": "26",
     "stroke": 15,
-    "hasGift": false
+    "hasGift": false,
+    "attendance": 2
   },
   {
     "name": "鄭人魁",
     "table": "22",
     "stroke": 15,
-    "hasGift": false
+    "hasGift": false,
+    "attendance": 4
   },
   {
     "name": "鄭岳輝",
     "table": "23",
     "stroke": 15,
-    "hasGift": false
+    "hasGift": false,
+    "attendance": 2
   },
   {
     "name": "鄭姿婷",
     "table": "27",
     "stroke": 15,
-    "hasGift": true
+    "hasGift": true,
+    "attendance": 1
   },
   {
     "name": "鄭壹壬",
     "table": "11",
     "stroke": 15,
-    "hasGift": false
+    "hasGift": false,
+    "attendance": 2
   },
   {
     "name": "賴毓成",
     "table": "16",
     "stroke": 16,
-    "hasGift": false
+    "hasGift": false,
+    "attendance": 2
   },
   {
     "name": "謝煥琴",
     "table": "21",
     "stroke": 17,
-    "hasGift": false
+    "hasGift": false,
+    "attendance": 2
   },
   {
     "name": "鐘諭欣",
     "table": "29",
     "stroke": 17,
-    "hasGift": false
+    "hasGift": false,
+    "attendance": 2
   },
   {
     "name": "顏上傑",
     "table": "13",
     "stroke": 18,
-    "hasGift": false
+    "hasGift": false,
+    "attendance": 1
   },
   {
     "name": "蘇祐麟",
     "table": "27",
     "stroke": 20,
-    "hasGift": false
+    "hasGift": false,
+    "attendance": 1
   },
   {
     "name": "蘇惠貞",
     "table": "16",
     "stroke": 20,
-    "hasGift": false
+    "hasGift": false,
+    "attendance": 2
   },
   {
     "name": "蘇鈺婷",
     "table": "27",
     "stroke": 20,
-    "hasGift": false
+    "hasGift": false,
+    "attendance": 2
   }
 ];
