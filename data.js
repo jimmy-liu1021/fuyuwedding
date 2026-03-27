aa404b6f761cd02fdef4e0641582a378b30b6a51
jimmy-liu1021 <80306173+jimmy-liu1021@users.noreply.github.com>
2026-03-28 01:09:46 +0800

    Add files via upload
    split data.js

---
+++ data.js
@@ -0,0 +1,872 @@
+﻿const data = [
+  {
+    "name": "孔正富",
+    "table": "3",
+    "stroke": 4,
+    "hasGift": false
+  },
+  {
+    "name": "孔紀秋梅",
+    "table": "主",
+    "stroke": 4,
+    "hasGift": false
+  },
+  {
+    "name": "孔麗芳",
+    "table": "8",
+    "stroke": 4,
+    "hasGift": false
+  },
+  {
+    "name": "王平安",
+    "table": "12",
+    "stroke": 4,
+    "hasGift": true
+  },
+  {
+    "name": "王俞婷",
+    "table": "29",
+    "stroke": 4,
+    "hasGift": true
+  },
+  {
+    "name": "王鈺",
+    "table": "28",
+    "stroke": 4,
+    "hasGift": true
+  },
+  {
+    "name": "王榮慶",
+    "table": "16",
+    "stroke": 4,
+    "hasGift": false
+  },
+  {
+    "name": "王德忠",
+    "table": "23",
+    "stroke": 4,
+    "hasGift": false
+  },
+  {
+    "name": "王錦松",
+    "table": "21",
+    "stroke": 4,
+    "hasGift": false
+  },
+  {
+    "name": "古榮昌",
+    "table": "10",
+    "stroke": 5,
+    "hasGift": false
+  },
+  {
+    "name": "朱一正",
+    "table": "20",
+    "stroke": 6,
+    "hasGift": false
+  },
+  {
+    "name": "朱展輝",
+    "table": "12",
+    "stroke": 6,
+    "hasGift": true
+  },
+  {
+    "name": "何駿惠",
+    "table": "16",
+    "stroke": 7,
+    "hasGift": false
+  },
+  {
+    "name": "余再得",
+    "table": "22",
+    "stroke": 7,
+    "hasGift": false
+  },
+  {
+    "name": "吳光裕",
+    "table": "20",
+    "stroke": 7,
+    "hasGift": false
+  },
+  {
+    "name": "吳志明",
+    "table": "1",
+    "stroke": 7,
+    "hasGift": false
+  },
+  {
+    "name": "吳孟玲",
+    "table": "28",
+    "stroke": 7,
+    "hasGift": true
+  },
+  {
+    "name": "吳秉鴻",
+    "table": "8",
+    "stroke": 7,
+    "hasGift": false
+  },
+  {
+    "name": "吳阿禮",
+    "table": "17",
+    "stroke": 7,
+    "hasGift": false
+  },
+  {
+    "name": "吳俊伯",
+    "table": "8",
+    "stroke": 7,
+    "hasGift": false
+  },
+  {
+    "name": "呂元庭",
+    "table": "9",
+    "stroke": 7,
+    "hasGift": false
+  },
+  {
+    "name": "呂佳霖",
+    "table": "6",
+    "stroke": 7,
+    "hasGift": false
+  },
+  {
+    "name": "呂昆憲",
+    "table": "3",
+    "stroke": 7,
+    "hasGift": false
+  },
+  {
+    "name": "呂翼純",
+    "table": "9",
+    "stroke": 7,
+    "hasGift": false
+  },
+  {
+    "name": "宋玉祥",
+    "table": "23",
+    "stroke": 7,
+    "hasGift": false
+  },
+  {
+    "name": "李正明",
+    "table": "25",
+    "stroke": 7,
+    "hasGift": false
+  },
+  {
+    "name": "李宗璇",
+    "table": "27",
+    "stroke": 7,
+    "hasGift": true
+  },
+  {
+    "name": "李承翰",
+    "table": "8",
+    "stroke": 7,
+    "hasGift": false
+  },
+  {
+    "name": "李昭鋒",
+    "table": "11",
+    "stroke": 7,
+    "hasGift": false
+  },
+  {
+    "name": "李啟宏",
+    "table": "15",
+    "stroke": 7,
+    "hasGift": true
+  },
+  {
+    "name": "李晴雯",
+    "table": "12",
+    "stroke": 7,
+    "hasGift": true
+  },
+  {
+    "name": "李毓祥",
+    "table": "12",
+    "stroke": 7,
+    "hasGift": true
+  },
+  {
+    "name": "李銘祥",
+    "table": "12",
+    "stroke": 7,
+    "hasGift": true
+  },
+  {
+    "name": "李曉雯",
+    "table": "29",
+    "stroke": 7,
+    "hasGift": true
+  },
+  {
+    "name": "辛佩娟",
+    "table": "13",
+    "stroke": 7,
+    "hasGift": true
+  },
+  {
+    "name": "周宗霖",
+    "table": "15",
+    "stroke": 8,
+    "hasGift": true
+  },
+  {
+    "name": "周濟忠",
+    "table": "9",
+    "stroke": 8,
+    "hasGift": false
+  },
+  {
+    "name": "明政車業",
+    "table": "23",
+    "stroke": 8,
+    "hasGift": false
+  },
+  {
+    "name": "林三義",
+    "table": "10",
+    "stroke": 8,
+    "hasGift": false
+  },
+  {
+    "name": "林士哲",
+    "table": "15",
+    "stroke": 8,
+    "hasGift": true
+  },
+  {
+    "name": "林禾穎",
+    "table": "21",
+    "stroke": 8,
+    "hasGift": false
+  },
+  {
+    "name": "林佑任",
+    "table": "13",
+    "stroke": 8,
+    "hasGift": true
+  },
+  {
+    "name": "林長入",
+    "table": "21",
+    "stroke": 8,
+    "hasGift": false
+  },
+  {
+    "name": "林啟章",
+    "table": "1",
+    "stroke": 8,
+    "hasGift": false
+  },
+  {
+    "name": "林達清",
+    "table": "17",
+    "stroke": 8,
+    "hasGift": false
+  },
+  {
+    "name": "邱業志",
+    "table": "13",
+    "stroke": 8,
+    "hasGift": true
+  },
+  {
+    "name": "侯凱文",
+    "table": "9",
+    "stroke": 9,
+    "hasGift": false
+  },
+  {
+    "name": "侯顯昇",
+    "table": "3",
+    "stroke": 9,
+    "hasGift": false
+  },
+  {
+    "name": "姚明宏",
+    "table": "17",
+    "stroke": 9,
+    "hasGift": false
+  },
+  {
+    "name": "封居和",
+    "table": "29",
+    "stroke": 9,
+    "hasGift": true
+  },
+  {
+    "name": "柯志雄",
+    "table": "19",
+    "stroke": 9,
+    "hasGift": false
+  },
+  {
+    "name": "柯河協",
+    "table": "19",
+    "stroke": 9,
+    "hasGift": false
+  },
+  {
+    "name": "洪秋菊",
+    "table": "20",
+    "stroke": 9,
+    "hasGift": false
+  },
+  {
+    "name": "洪啟聰",
+    "table": "11",
+    "stroke": 9,
+    "hasGift": false
+  },
+  {
+    "name": "洪慶昌",
+    "table": "10",
+    "stroke": 9,
+    "hasGift": false
+  },
+  {
+    "name": "紀素蘭",
+    "table": "3",
+    "stroke": 9,
+    "hasGift": false
+  },
+  {
+    "name": "翁晉源",
+    "table": "26",
+    "stroke": 10,
+    "hasGift": false
+  },
+  {
+    "name": "徐紹謙",
+    "table": "9",
+    "stroke": 10,
+    "hasGift": false
+  },
+  {
+    "name": "張永林",
+    "table": "主",
+    "stroke": 11,
+    "hasGift": false
+  },
+  {
+    "name": "張宇仁",
+    "table": "11",
+    "stroke": 11,
+    "hasGift": false
+  },
+  {
+    "name": "張孝瑄",
+    "table": "5",
+    "stroke": 11,
+    "hasGift": true
+  },
+  {
+    "name": "張秀屏",
+    "table": "5",
+    "stroke": 11,
+    "hasGift": false
+  },
+  {
+    "name": "張育誠",
+    "table": "3",
+    "stroke": 11,
+    "hasGift": false
+  },
+  {
+    "name": "張宗德",
+    "table": "18",
+    "stroke": 11,
+    "hasGift": false
+  },
+  {
+    "name": "張美淑",
+    "table": "10",
+    "stroke": 11,
+    "hasGift": false
+  },
+  {
+    "name": "張書鳴",
+    "table": "7",
+    "stroke": 11,
+    "hasGift": false
+  },
+  {
+    "name": "張國勤",
+    "table": "10",
+    "stroke": 11,
+    "hasGift": false
+  },
+  {
+    "name": "張景崑",
+    "table": "18",
+    "stroke": 11,
+    "hasGift": false
+  },
+  {
+    "name": "張曉萍",
+    "table": "1",
+    "stroke": 11,
+    "hasGift": false
+  },
+  {
+    "name": "莊永來",
+    "table": "11",
+    "stroke": 11,
+    "hasGift": false
+  },
+  {
+    "name": "莊祐齊",
+    "table": "28",
+    "stroke": 11,
+    "hasGift": true
+  },
+  {
+    "name": "許有發",
+    "table": "25",
+    "stroke": 11,
+    "hasGift": false
+  },
+  {
+    "name": "許志偉",
+    "table": "12",
+    "stroke": 11,
+    "hasGift": true
+  },
+  {
+    "name": "郭盈瑩",
+    "table": "13",
+    "stroke": 11,
+    "hasGift": true
+  },
+  {
+    "name": "郭貞妤",
+    "table": "6",
+    "stroke": 11,
+    "hasGift": false
+  },
+  {
+    "name": "郭慶松",
+    "table": "22",
+    "stroke": 11,
+    "hasGift": false
+  },
+  {
+    "name": "陳文英",
+    "table": "5",
+    "stroke": 11,
+    "hasGift": true
+  },
+  {
+    "name": "陳正忠",
+    "table": "18",
+    "stroke": 11,
+    "hasGift": false
+  },
+  {
+    "name": "陳民智",
+    "table": "25",
+    "stroke": 11,
+    "hasGift": false
+  },
+  {
+    "name": "陳永洲",
+    "table": "19",
+    "stroke": 11,
+    "hasGift": false
+  },
+  {
+    "name": "陳俊男",
+    "table": "20",
+    "stroke": 11,
+    "hasGift": false
+  },
+  {
+    "name": "陳奕臻",
+    "table": "28",
+    "stroke": 11,
+    "hasGift": true
+  },
+  {
+    "name": "陳柏亨",
+    "table": "28",
+    "stroke": 11,
+    "hasGift": true
+  },
+  {
+    "name": "陳素貞",
+    "table": "1",
+    "stroke": 11,
+    "hasGift": false
+  },
+  {
+    "name": "陳聖馗",
+    "table": "12",
+    "stroke": 11,
+    "hasGift": true
+  },
+  {
+    "name": "陳裕生",
+    "table": "15",
+    "stroke": 11,
+    "hasGift": true
+  },
+  {
+    "name": "彭永富",
+    "table": "18",
+    "stroke": 12,
+    "hasGift": false
+  },
+  {
+    "name": "游天下",
+    "table": "2",
+    "stroke": 12,
+    "hasGift": false
+  },
+  {
+    "name": "游志成",
+    "table": "2",
+    "stroke": 12,
+    "hasGift": false
+  },
+  {
+    "name": "游武發",
+    "table": "主",
+    "stroke": 12,
+    "hasGift": false
+  },
+  {
+    "name": "游俊勝",
+    "table": "7",
+    "stroke": 12,
+    "hasGift": false
+  },
+  {
+    "name": "游俊惠",
+    "table": "2",
+    "stroke": 12,
+    "hasGift": false
+  },
+  {
+    "name": "游峻林",
+    "table": "2",
+    "stroke": 12,
+    "hasGift": false
+  },
+  {
+    "name": "游淑芬",
+    "table": "11",
+    "stroke": 12,
+    "hasGift": false
+  },
+  {
+    "name": "湯有成",
+    "table": "20",
+    "stroke": 12,
+    "hasGift": false
+  },
+  {
+    "name": "黃世雄",
+    "table": "26",
+    "stroke": 12,
+    "hasGift": false
+  },
+  {
+    "name": "黃成平",
+    "table": "26",
+    "stroke": 12,
+    "hasGift": false
+  },
+  {
+    "name": "黃秋美",
+    "table": "16",
+    "stroke": 12,
+    "hasGift": false
+  },
+  {
+    "name": "黃美芬",
+    "table": "13",
+    "stroke": 12,
+    "hasGift": true
+  },
+  {
+    "name": "黃美珍",
+    "table": "20",
+    "stroke": 12,
+    "hasGift": false
+  },
+  {
+    "name": "黃振鈞",
+    "table": "主",
+    "stroke": 12,
+    "hasGift": false
+  },
+  {
+    "name": "黃健航",
+    "table": "15",
+    "stroke": 12,
+    "hasGift": true
+  },
+  {
+    "name": "黃崇華",
+    "table": "6",
+    "stroke": 12,
+    "hasGift": false
+  },
+  {
+    "name": "黃意婷",
+    "table": "27",
+    "stroke": 12,
+    "hasGift": true
+  },
+  {
+    "name": "黃福源",
+    "table": "17",
+    "stroke": 12,
+    "hasGift": false
+  },
+  {
+    "name": "黃馨儀",
+    "table": "27",
+    "stroke": 12,
+    "hasGift": true
+  },
+  {
+    "name": "楊王元廷",
+    "table": "28",
+    "stroke": 13,
+    "hasGift": true
+  },
+  {
+    "name": "楊保生",
+    "table": "17",
+    "stroke": 13,
+    "hasGift": false
+  },
+  {
+    "name": "楊益彰",
+    "table": "1",
+    "stroke": 13,
+    "hasGift": false
+  },
+  {
+    "name": "楊清雄",
+    "table": "19",
+    "stroke": 13,
+    "hasGift": false
+  },
+  {
+    "name": "楊智捷",
+    "table": "15",
+    "stroke": 13,
+    "hasGift": true
+  },
+  {
+    "name": "葉永輝",
+    "table": "17",
+    "stroke": 13,
+    "hasGift": false
+  },
+  {
+    "name": "賈志浩",
+    "table": "29",
+    "stroke": 13,
+    "hasGift": true
+  },
+  {
+    "name": "鄒嘉棋",
+    "table": "16",
+    "stroke": 13,
+    "hasGift": false
+  },
+  {
+    "name": "廖啟亨",
+    "table": "28",
+    "stroke": 14,
+    "hasGift": true
+  },
+  {
+    "name": "廖燈村",
+    "table": "21",
+    "stroke": 14,
+    "hasGift": false
+  },
+  {
+    "name": "劉文能",
+    "table": "25",
+    "stroke": 15,
+    "hasGift": false
+  },
+  {
+    "name": "劉志宗",
+    "table": "18",
+    "stroke": 15,
+    "hasGift": false
+  },
+  {
+    "name": "劉邦宏",
+    "table": "主",
+    "stroke": 15,
+    "hasGift": false
+  },
+  {
+    "name": "劉佳偉",
+    "table": "5",
+    "stroke": 15,
+    "hasGift": true
+  },
+  {
+    "name": "劉晉華",
+    "table": "8",
+    "stroke": 15,
+    "hasGift": false
+  },
+  {
+    "name": "劉婉苓",
+    "table": "8",
+    "stroke": 15,
+    "hasGift": false
+  },
+  {
+    "name": "劉達德",
+    "table": "19",
+    "stroke": 15,
+    "hasGift": false
+  },
+  {
+    "name": "劉德陽",
+    "table": "5",
+    "stroke": 15,
+    "hasGift": false
+  },
+  {
+    "name": "劉興漢",
+    "table": "6",
+    "stroke": 15,
+    "hasGift": false
+  },
+  {
+    "name": "劉馥瑩",
+    "table": "5",
+    "stroke": 15,
+    "hasGift": false
+  },
+  {
+    "name": "歐進雄",
+    "table": "26",
+    "stroke": 15,
+    "hasGift": false
+  },
+  {
+    "name": "蔡正昌",
+    "table": "6",
+    "stroke": 15,
+    "hasGift": false
+  },
+  {
+    "name": "蔡明信",
+    "table": "15",
+    "stroke": 15,
+    "hasGift": true
+  },
+  {
+    "name": "蔡昕翰",
+    "table": "27",
+    "stroke": 15,
+    "hasGift": true
+  },
+  {
+    "name": "蔡啟明",
+    "table": "21",
+    "stroke": 15,
+    "hasGift": false
+  },
+  {
+    "name": "蔡智閱",
+    "table": "13",
+    "stroke": 15,
+    "hasGift": true
+  },
+  {
+    "name": "蔡瑞珍",
+    "table": "13",
+    "stroke": 15,
+    "hasGift": true
+  },
+  {
+    "name": "蔣瑞融",
+    "table": "26",
+    "stroke": 15,
+    "hasGift": false
+  },
+  {
+    "name": "鄭人魁",
+    "table": "22",
+    "stroke": 15,
+    "hasGift": false
+  },
+  {
+    "name": "鄭岳輝",
+    "table": "23",
+    "stroke": 15,
+    "hasGift": false
+  },
+  {
+    "name": "鄭姿婷",
+    "table": "27",
+    "stroke": 15,
+    "hasGift": true
+  },
+  {
+    "name": "鄭壹壬",
+    "table": "11",
+    "stroke": 15,
+    "hasGift": false
+  },
+  {
+    "name": "賴毓成",
+    "table": "16",
+    "stroke": 16,
+    "hasGift": false
+  },
+  {
+    "name": "謝煥琴",
+    "table": "21",
+    "stroke": 17,
+    "hasGift": false
+  },
+  {
+    "name": "鐘諭欣",
+    "table": "29",
+    "stroke": 17,
+    "hasGift": false
+  },
+  {
+    "name": "顏上傑",
+    "table": "13",
+    "stroke": 18,
+    "hasGift": false
+  },
+  {
+    "name": "蘇祐麟",
+    "table": "27",
+    "stroke": 20,
+    "hasGift": false
+  },
+  {
+    "name": "蘇惠貞",
+    "table": "16",
+    "stroke": 20,
+    "hasGift": false
+  },
+  {
+    "name": "蘇鈺婷",
+    "table": "27",
+    "stroke": 20,
+    "hasGift": false
+  }
+];
